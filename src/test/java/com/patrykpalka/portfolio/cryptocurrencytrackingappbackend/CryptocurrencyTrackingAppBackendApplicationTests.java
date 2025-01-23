0c937f3e5f7b43d8dc25bdeecf229b9cf40490ff
patrykpalka <patryk-palka2@wp.pl>
2025-01-23 16:55:21 +0100

    Initial commit

---
+++ src/test/java/com/patrykpalka/portfolio/cryptocurrencytrackingappbackend/CryptocurrencyTrackingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrykpalka.portfolio.cryptocurrencytrackingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyTrackingAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
