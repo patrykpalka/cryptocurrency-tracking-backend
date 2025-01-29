f49bfed5774710f31335b5569d4638d41b11fef7
patrykpalka <patryk-palka2@wp.pl>
2025-01-29 23:04:27 +0100

    Refactor: Improve null handling with Optional

--- src/main/java/com/patrykpalka/portfolio/cryptotracker/backend/service/CryptoService.java
+++ src/main/java/com/patrykpalka/portfolio/cryptotracker/backend/service/CryptoService.java
@@ -1,12 +1,14 @@
 package com.patrykpalka.portfolio.cryptotracker.backend.service;
 
-import com.patrykpalka.portfolio.cryptotracker.backend.dto.MarketDataApiResponseDTO;
 import com.patrykpalka.portfolio.cryptotracker.backend.dto.CryptoPricesResponseDTO;
+import com.patrykpalka.portfolio.cryptotracker.backend.dto.MarketDataApiResponseDTO;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,20 +21,17 @@ public class CryptoService {
     }
 
     public List<CryptoPricesResponseDTO> getCryptoPrices(String symbols, String currency) {
-        String currencyLowerCase = currency.toLowerCase();
-        String urlPath = "/coins/markets?vs_currency=" + currencyLowerCase;
+        String urlPath = "/coins/markets?vs_currency=" + currency.toLowerCase();
 
-        List<MarketDataApiResponseDTO> marketDataApiRespons = cryptoApiClient.get()
+        List<MarketDataApiResponseDTO> marketDataApiResponse = cryptoApiClient.get()
                 .uri(urlPath + (symbols != null ? "&ids=" + symbols : ""))
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<List<MarketDataApiResponseDTO>>() {})
                 .block();
 
-        if (marketDataApiRespons == null) {
-            return List.of();
-        }
-
-        return marketDataApiRespons.stream()
+        return Optional.ofNullable(marketDataApiResponse)
+                .orElseGet(Collections::emptyList)
+                .stream()
                 .map(dto -> new CryptoPricesResponseDTO(
                         dto.symbol().toUpperCase(),
                         dto.currentPrice(),
