82ff734d63ffed52dc29e03a2e3b0d5829bd2d16
patrykpalka <patryk-palka2@wp.pl>
2025-02-05 23:38:31 +0100

    Feat: Add CoinMarketDataResponseDTO

---
+++ src/main/java/com/patrykpalka/portfolio/cryptotracker/backend/dto/CoinMarketDataResponseDTO.java
@@ -0,0 +1,19 @@
+package com.patrykpalka.portfolio.cryptotracker.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record CoinMarketDataResponseDTO(
+        String symbol,
+
+        @JsonProperty("market_cap")
+        long marketCap,
+
+        @JsonProperty("24h_volume")
+        long volume24h,
+
+        @JsonProperty("circulating_supply")
+        long circulatingSupply,
+
+        String currency
+) {
+}
