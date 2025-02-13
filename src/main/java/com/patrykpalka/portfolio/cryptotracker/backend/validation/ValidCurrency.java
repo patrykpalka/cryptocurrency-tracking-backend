c07cfb257086bf6fbdec5c4cf7f2f4a63c5b7c39
patrykpalka <patryk-palka2@wp.pl>
2025-02-13 22:34:30 +0100

    Feat add ValidCurrency annotation

---
+++ src/main/java/com/patrykpalka/portfolio/cryptotracker/backend/validation/ValidCurrency.java
@@ -0,0 +1,15 @@
+package com.patrykpalka.portfolio.cryptotracker.backend.validation;
+
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(CurrencyValidator.class)
+public @interface ValidCurrency {
+    String message() default "Invalid currency";
+}
